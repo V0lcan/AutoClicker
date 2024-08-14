b32f79fa5ad15b6760e121be598382746e88307f
Albin Sandefalk <albinsand@hotmail.se>
2024-08-14 18:39:27 +0200

    v1.0 done

---
+++ Autoclicker.py
@@ -0,0 +1,70 @@
+import sys, clicker
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Volcan's Autoclicker")
+        self.setGeometry(100, 100, 400, 200)
+        self.setFixedSize(self.width(), self.height())
+        self.setWindowIcon(QIcon("./assets/icon.png"))
+        self.setStyleSheet("background-image: url(./assets/main_bg.jpg);")
+
+        self.initUI()
+
+    def initUI(self):
+
+        # Label for the main title
+        main_label = QLabel("Volcan's Autoclicker", self)
+        main_label.setFont(QFont('Arial', 14))
+        main_label.setGeometry(100, 10, 200, 30)
+        main_label.setStyleSheet("color: white;"
+                            "padding: 5px;")
+        main_label.setAlignment(Qt.AlignCenter)
+
+        # Label for the delay
+        delay_label = QLabel("Delay (ms):", self)
+        delay_label.setFont(QFont('Arial', 10))
+        delay_label.setGeometry(50, 50, 100, 30)
+        delay_label.setStyleSheet("color: white;"
+                            "padding: 5px;")
+        
+        # Input field for the delay
+        self.delay_input = QLineEdit(self)
+        self.delay_input.setGeometry(150, 50, 100, 30)
+        self.delay_input.setStyleSheet("color: white;"
+                            "padding: 5px;"
+                            "border: 1px solid #292929;"
+                            "font-size: 12px;")
+        
+        # Label for the window select
+        instruction_label = QLabel("When the program is running press O to toggle\nor P to stop the autoclicker.", self)
+        instruction_label.setFont(QFont('Arial', 10))
+        instruction_label.setGeometry(60, 90, 300, 50)
+        instruction_label.setStyleSheet("color: white;"
+                    "padding: 0px;")
+        instruction_label.setAlignment(Qt.AlignCenter)
+        
+        # Start buttonq
+        start_button = QPushButton("Start", self)
+        start_button.setGeometry(150, 150, 100, 30)
+        start_button.setStyleSheet("color: white;"
+                            "padding: 5px;")
+        start_button.clicked.connect(self.start_on_click)
+
+    # Function to start the autoclicker
+    def start_on_click(self):
+        ms = self.delay_input.text()
+        clicker.clicker(int(ms))
+
+def Main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    Main()
---
+++ clicker.py
@@ -0,0 +1,52 @@
+import time, threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+def clicker(delay_input):
+
+    mouse = Controller()
+    m1 = Button.left
+    autoclick_toggle = KeyCode(char='o')
+    autoclick_exit = KeyCode(char='p')
+    delay = delay_input / 1000
+
+    class autoclicker(threading.Thread):
+        def __init__(self, delay, button):
+            super().__init__()
+            self.delay = delay
+            self.button = button
+            self.running = False
+            self.program_running = True
+
+        def start_clicking(self):
+            self.running = True
+
+        def stop_clicking(self):
+            self.running = False
+
+        def exit(self):
+            self.stop_clicking()
+            self.program_running = False
+
+        def run(self):
+            while self.program_running:
+                while self.running:
+                    mouse.click(self.button)
+                    time.sleep(self.delay)
+                time.sleep(0.1)
+
+    click_thread = autoclicker(delay, m1)
+    click_thread.start()
+
+    def on_press(key):
+        if key == autoclick_toggle:
+            if click_thread.running:
+                click_thread.stop_clicking()
+            else:
+                click_thread.start_clicking()
+        elif key == autoclick_exit:
+            click_thread.exit()
+            listener.stop()
+
+    with Listener(on_press=on_press) as listener:
+        listener.join()
