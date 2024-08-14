61d6ecb640e3773f5adb344df58fbd042923f2e6
Albin Sandefalk <albinsand@hotmail.se>
2024-08-14 20:24:15 +0200

    Removed double title

--- Autoclicker.py
+++ Autoclicker.py
@@ -11,7 +11,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Volcan's Autoclicker")
-        self.setGeometry(100, 100, 400, 200)
+        self.setGeometry(100, 100, 400, 160)
         self.setFixedSize(self.width(), self.height())
         self.setWindowIcon(QIcon("./assets/icon.png"))
         self.setStyleSheet("background-image: url(./assets/main_bg.jpg);")
@@ -20,24 +20,16 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
 
-        # Label for the main title
-        main_label = QLabel("Volcan's Autoclicker", self)
-        main_label.setFont(QFont('Arial', 14))
-        main_label.setGeometry(100, 10, 200, 30)
-        main_label.setStyleSheet("color: white;"
-                                 "padding: 5px;")
-        main_label.setAlignment(Qt.AlignCenter)
-
         # Label for the delay
         delay_label = QLabel("Delay (ms):", self)
         delay_label.setFont(QFont('Arial', 10))
-        delay_label.setGeometry(50, 50, 100, 30)
+        delay_label.setGeometry(50, 15, 100, 30)
         delay_label.setStyleSheet("color: white;"
                                   "padding: 5px;")
         
         # Input field for the delay
         self.delay_input = QLineEdit(self)
-        self.delay_input.setGeometry(150, 50, 100, 30)
+        self.delay_input.setGeometry(150, 15, 100, 30)
         self.delay_input.setStyleSheet("color: white;"
                                        "padding: 5px;"
                                        "border: 1px solid #292929;"
@@ -46,14 +38,14 @@ class MainWindow(QMainWindow):
         # Label for the window select
         instruction_label = QLabel("When the program is running press O to toggle\nor P to stop the autoclicker.", self)
         instruction_label.setFont(QFont('Arial', 10))
-        instruction_label.setGeometry(60, 90, 300, 50)
+        instruction_label.setGeometry(60, 50, 300, 50)
         instruction_label.setStyleSheet("color: white;"
                                         "padding: 0px;")
         instruction_label.setAlignment(Qt.AlignCenter)
         
         # Start buttonq
         start_button = QPushButton("Start", self)
-        start_button.setGeometry(150, 150, 100, 30)
+        start_button.setGeometry(150, 110, 100, 30)
         start_button.setStyleSheet("color: white;"
                                    "padding: 5px;")
         start_button.clicked.connect(self.start_on_click)
